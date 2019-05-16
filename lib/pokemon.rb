f6897f84f95084b3dcb8cf2e8f63e285d26f9a54
Ricardo Paul <kingupper0@gmail.com>
2019-05-16 18:56:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+  attr_accessor :name
+  
+    name = get_pokemon_name_from(node)
+    type = get_pokemon_type_from(node)
+    
+  def initialize(name, type, db)
+    @db = db
+  end
+  
 end
