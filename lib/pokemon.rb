48435c91031b64e70bd98893f279f42050814bda
Ricardo Paul <kingupper0@gmail.com>
2019-05-16 19:08:24 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
    # name = get_pokemon_name_from(node)
     # type = get_pokemon_type_from(node)
     
-  def initialize(name, type, db, hp)
+  def initialize(name:, type:, db:, hp:)
     @name = name
     @type = type
     @db = db
