2b71f8298d91189363a112d35fd6fc0c7b6bd2d7
Ricardo Paul <kingupper0@gmail.com>
2019-05-16 19:19:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
   end
   
   def self.find(db, id_number)
-    db.execute("SELECT * FROM Pokemon WHERE id=?", id_number).first
+    Pokemon_find = db.execute("SELECT * FROM Pokemon WHERE id=?", id_number).first
     
   end
   
